6c609a4bd89698ed301d6551a503b6cc23343011
Semih <smhmmc98@gmail.com>
2021-12-07 20:39:59 +0300

    Update userscript.js

--- WikiSummary/userscript.js
+++ WikiSummary/userscript.js
@@ -12,27 +12,37 @@
 
 let API_Key = 'ENTER_API_KEY_HERE'; //Replace ENTER_API_KEY_HERE with your API Key (so keep the quote marks)
 let MaximumFactMessageLength = 420; // Messages can exceed this limit in version 0.1
+let MaximumFactsCountToDisplay = 3; // Maximum amount of facts to display
 
 let API_URL = "https://api.bigdatacloud.net/data/reverse-geocode?localityLanguage=en&"
 let WIKI_URL = "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&origin=*&titles="
 
 let checked = parseInt(sessionStorage.getItem("FactLocationChecked"), 10);
-let last_fact =
-    {
-        "link": "<link_not_provided>",
-        "text": "nothing interesting"
-    }
+let facts = []
+
+let INVALIDLINK = "#";
 
 if(sessionStorage.getItem("FactLocationChecked") == null) {
     sessionStorage.setItem("FactLocationChecked", 0);
     checked = 0;
 };
 
+const DEBUG_ENABLED = false
+
+function debug(obj)
+{
+    if(DEBUG_ENABLED) console.log(obj)
+}
+
 function cleanPages(pages)
 {
     let exclude = [-2, -1,// Missing or invalid
+                   /* TO-DO: Maybe exclude
                    83759,// USA
                    13530298,// UK
+                   1223848,// North EU Plain
+                   1145843,// Atlantic Coastal Plain
+                   */
                   ]
     exclude.forEach(idx => idx in pages ? delete pages[idx] : null);
 }
@@ -43,21 +53,28 @@ function setNameToPostal(obj, name)
     obj.description = name
 }
 
+function styleFact(name,desc)
+{
+    return `<h3 style="color: green">${name}</h3>${desc}`
+}
+
 function getTitlesFromLocation()
 {
     return getLocationObject()
     .then(loc => {
         needsWiki = true;
         let infos = loc.localityInfo.informative;
+        debug(infos)
 
         let len = Object.keys(infos).length;
         if (len == 1)
         {
+            console.log("length 1")
             let info = infos[0]
             if (info.order < 3)
             {
                 needsWiki = false;
-                return info.description
+                return styleFact(info.name, info.description)
             }
             return info;
         }
@@ -68,15 +85,11 @@ function getTitlesFromLocation()
 
             if (filtered.length == 0)
             {
-                if (filtered.length == 1)
+                if (infos.length >= 1)
                 {
                     needsWiki = false;
-                    return infos[0].name + ": " + infos[0].description;
-                }
-                else if (filtered.length == 2)
-                {
-                    needsWiki = false;
-                    return infos[1].name + ": " + infos[1].description;
+                    facts = infos.map(obj => { return {"text": styleFact(obj.name, obj.description), "link": INVALIDLINK} })
+                    return null;
                 }
                 else
                 {
@@ -123,49 +136,66 @@ function getFactFromTitles(titles)
         .then(res => res.json())
         .then(result =>{
 
+        facts = []
         let pages = result.query.pages;
         cleanPages(pages);
+        debug(pages)
 
         let keys = Object.keys(pages);
         if (keys.length == 0) return null
 
-        let k = keys[Math.floor(Math.random() * keys.length)];
-        let fact = pages[k];
-        if (fact == null) return null
+        let del = 0;
 
-        let reduced = fact.extract;
-        while (reduced.endsWith("may refer to:\n\n"))
+        keys.forEach(k =>
         {
-            delete pages[k]
+            if (facts.length >= MaximumFactsCountToDisplay) return null
 
-            keys = Object.keys(pages);
-            k = keys[Math.floor(Math.random() * keys.length)];
-
-            fact = pages[k];
+            let fact = pages[k];
             if (fact == null) return null
 
-            reduced = fact.extract;
-            if (reduced == null) return null
-        }
-
-        if (reduced.length > MaximumFactMessageLength)
-        {
-            let paragraphs = reduced.split(". ")
-            let i = 0
-            reduced = paragraphs.length != 0 ? "" : reduced
-
-            while (i < paragraphs.length && reduced.length <= MaximumFactMessageLength)
+            let reduced = fact.extract;
+            reduced = reduced.trimEnd("\n")
+            while (reduced.endsWith("refer to:"))
             {
-                reduced = reduced + ". " + paragraphs[i]
-                i++;
+                delete pages[k]
+
+                keys = Object.keys(pages);
+                k = keys[Math.floor(Math.random() * keys.length)];
+
+                fact = pages[k];
+                if (fact == null) return null
+
+                reduced = fact.extract;
+                if (reduced == null) return null
+
+                reduced = reduced.trimEnd("\n")
             }
 
-            // TO-DO: Add "read more" button
-            //reduced = reduced.slice(0,MaximumFactMessageLength) + "..."
-        }
+            if (reduced.length > MaximumFactMessageLength)
+            {
+                let paragraphs = reduced.split(". ")
+                let j = 0
+                reduced = paragraphs.length != 0 ? "" : reduced
 
-        last_fact.link = "https://en.wikipedia.org/?curid=" + fact.pageid;
-        return fact.title + ": " + reduced;
+                while (j < paragraphs.length && reduced.length <= MaximumFactMessageLength)
+                {
+                    reduced = reduced + ". " + paragraphs[j]
+                    j++;
+                }
+
+                // TO-DO: Add "read more" button
+                //reduced = reduced.slice(0,MaximumFactMessageLength) + "..."
+            }
+
+            let f = {
+                "text": styleFact(fact.title, reduced),
+                "link": "https://en.wikipedia.org/?curid=" + fact.pageid
+            }
+            debug(f)
+            facts.push(f)
+        })
+
+        return facts;
     });
 }
 
@@ -177,44 +207,49 @@ function SetDisplayFact()
         .then(titles => {
         if (needsWiki)
         {
-            getFactFromTitles(titles).then(fact => {
-                if (fact == null)
+            getFactFromTitles(titles).then(facts => {
+                if (facts == null)
                 {
-                    fact = titles.name + ": " + titles.description
+                    facts = [
+                        {
+                            "text": styleFact(titles.name, titles.description),
+                            "link": INVALIDLINK
+                        }
+                    ]
                 }
-
-                last_fact.text = fact;
-                document.getElementById("location-fact").innerHTML = getLastFactInnerHtml() + "<br>" + getLastFactLinkHtml();
+                setFactInnerHtml();
             })
         }
         else
         {
             needsWiki = true;
-            last_fact.text = titles;
-            last_fact.link = "<link_not_provided>";
-            document.getElementById("location-fact").innerHTML = getLastFactInnerHtml() + "<br>" + getLastFactLinkHtml();
+            facts = [
+                        {
+                            "text": titles,
+                            "link": INVALIDLINK
+                        }
+            ]
+            setFactInnerHtml();
         }
     })
 }
 
-function getLastFactLinkHtml()
+function setFactInnerHtml()
 {
-    return last_fact.link == "<link_not_provided>"
-        ? ""
-        : `<br><u><a href="${last_fact.link}"; style="color: white"><i><h2>Source Page</h2></i><a/></u>`;
-}
+    let str = facts
+    .map((fact,i) => {
+         return `<br><h2 style="color: orange">Fact ${i+1}</h2>(<u><a href="${fact.link}"; style="color: white"><i>source</i></a></u>)<br>${fact.text.split(". ").reduce((prev, curr) => prev + "<br>" + curr)}`
+        })
+    .join("<hr>")
 
-function getLastFactInnerHtml()
-{
-    let text = last_fact.text.split(". ").reduce((prev, curr) => prev + "<br>" + curr);
-    return `<br><u><i><h2>Fact</h2></i></u>${text}`;
+    document.getElementById("location-fact").innerHTML = str;
 }
 
 function factAttempt1(newDiv1) {
     if(document.getElementById("location-fact") == null && document.getElementsByClassName("round-result_distanceDescription__13lR1").length == 1 && location.pathname.startsWith("/game/")) {
         newDiv1 = document.createElement("div")
         document.getElementsByClassName("round-result_distanceDescription__13lR1")[0].appendChild(newDiv1);
-        newDiv1.innerHTML = `<div id="location-fact" style="text-align:center">${getLastFactInnerHtml()}</div>`;
+        newDiv1.innerHTML = `<div id="location-fact" style="text-align:center">nothing interesting...</div>`;
     };
 };
 
